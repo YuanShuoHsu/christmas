f89b3964ad769d9d1771dcaebce3f7b0b97f1177
YuanShuoHsu <ook1725@gmail.com>
2025-12-17 17:14:51 +0800

    Refactor ChristmasTree and TreeLayers components to streamline state management by directly accessing the ChristmasTreeStore; simplify props handling and improve code readability.

--- components/ChristmasTree/TreeLayers/index.tsx
+++ components/ChristmasTree/TreeLayers/index.tsx
@@ -1,39 +1,39 @@
 import TreeOrnaments from "./TreeOrnaments";
 
 import { faceIndices, layerIndices } from "@/constants/christmasTree";
+import { useChristmasTreeStore } from "@/stores/christmasTreeStoreProvider";
 
 import { cssVar } from "@/utils/cssVar";
-import type { TreeOrnament } from "@/utils/treeOrnaments";
 
-interface TreeLayersProps {
-  ornaments: TreeOrnament[];
-  photoUrl: string | null;
-}
+const TreeLayers = () => {
+  const ornaments = useChristmasTreeStore((state) => state.ornaments);
+  const photoUrl = useChristmasTreeStore((state) => state.photoUrl);
 
-const TreeLayers = ({ ornaments, photoUrl }: TreeLayersProps) => (
-  <>
-    {layerIndices.map((j) => (
-      <div
-        key={j}
-        className="absolute top-0 left-0 w-full h-full transform-3d transform-[translateY(calc(100px*var(--j)))]"
-        style={cssVar("--j", j)}
-      >
-        {faceIndices.map((i) => (
-          <span
-            key={i}
-            aria-hidden="true"
-            className="absolute top-0 left-0 w-full h-full bg-linear-to-r from-[#69c069] to-[#77dd77] [clip-path:polygon(50%_0%,0%_100%,100%_100%)] origin-bottom border-b-10 border-b-[#00000019] transform-[rotateY(calc(90deg*var(--i)))_rotateX(30deg)_translateZ(173px)]"
-            style={cssVar("--i", i)}
+  return (
+    <>
+      {layerIndices.map((j) => (
+        <div
+          key={j}
+          className="absolute top-0 left-0 w-full h-full transform-3d transform-[translateY(calc(100px*var(--j)))]"
+          style={cssVar("--j", j)}
+        >
+          {faceIndices.map((i) => (
+            <span
+              key={i}
+              aria-hidden="true"
+              className="absolute top-0 left-0 w-full h-full bg-linear-to-r from-[#69c069] to-[#77dd77] [clip-path:polygon(50%_0%,0%_100%,100%_100%)] origin-bottom border-b-10 border-b-[#00000019] transform-[rotateY(calc(90deg*var(--i)))_rotateX(30deg)_translateZ(173px)]"
+              style={cssVar("--i", i)}
+            />
+          ))}
+          <TreeOrnaments
+            photoUrl={photoUrl}
+            ornaments={ornaments}
+            layerIndex={j}
           />
-        ))}
-        <TreeOrnaments
-          photoUrl={photoUrl}
-          ornaments={ornaments}
-          layerIndex={j}
-        />
-      </div>
-    ))}
-  </>
-);
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default TreeLayers;
--- components/ChristmasTree/index.tsx
+++ components/ChristmasTree/index.tsx
@@ -4,23 +4,16 @@ import TreeLayers from "@/components/ChristmasTree/TreeLayers";
 import TreeShadow from "@/components/ChristmasTree/TreeShadow";
 import TreeTrunk from "@/components/ChristmasTree/TreeTrunk";
 
-import { useChristmasTreeStore } from "@/stores/christmasTreeStoreProvider";
-
-const ChristmasTree = () => {
-  const ornaments = useChristmasTreeStore((state) => state.ornaments);
-  const photoUrl = useChristmasTreeStore((state) => state.photoUrl);
-
-  return (
-    <div
-      aria-label="3D Christmas tree"
-      className="relative top-[-150px] w-[300px] h-[300px] transform-3d before:content-['⭐'] before:absolute before:top-[-120px] before:left-[calc(50%-30px)] before:text-[4em] transform-[rotateX(-20deg)_rotateY(30deg)] animate-tree-spin motion-reduce:animate-none"
-      role="img"
-    >
-      <TreeLayers photoUrl={photoUrl} ornaments={ornaments} />
-      <TreeTrunk />
-      <TreeShadow />
-    </div>
-  );
-};
+const ChristmasTree = () => (
+  <div
+    aria-label="3D Christmas tree"
+    className="relative top-[-150px] w-[300px] h-[300px] transform-3d before:content-['⭐'] before:absolute before:top-[-120px] before:left-[calc(50%-30px)] before:text-[4em] transform-[rotateX(-20deg)_rotateY(30deg)] animate-tree-spin motion-reduce:animate-none"
+    role="img"
+  >
+    <TreeLayers />
+    <TreeTrunk />
+    <TreeShadow />
+  </div>
+);
 
 export default ChristmasTree;
